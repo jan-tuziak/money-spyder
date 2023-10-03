69aff3d79c9ed37b85f9f95d3f99689c221a488c
Jan Tuziak <jan.tuziak@gmail.com>
2023-10-03 16:58:00 +0200

    init with proper template

--- money-spyder/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace money_spyder.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
